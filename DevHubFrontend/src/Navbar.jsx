249008b6f20cfb6719b6082b30aa2e709dc1693f
Rishab Jha <rishabjha134@gmail.com>
2025-04-25 21:32:13 +0530

    remove unnecessary things

--- DevHubFrontend/src/Navbar.jsx
+++ DevHubFrontend/src/Navbar.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { removeUser } from "./utils/userSlice";
