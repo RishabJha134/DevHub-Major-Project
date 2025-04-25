ed5f5751cb9f58171c293bef71c80255d72f2a74
Rishab Jha <rishabjha134@gmail.com>
2025-04-25 13:35:47 +0530

    increase speed

--- DevHubFrontend/src/utils/socket.jsx
+++ DevHubFrontend/src/utils/socket.jsx
@@ -3,11 +3,16 @@ import io from "socket.io-client";
 
 export const createSocketConnection = () => {
   return io("https://devmatch-major-project.onrender.com", {
-    withCredentials: true,
-    reconnection: true,
-    reconnectionAttempts: 5,
-    reconnectionDelay: 1000,
-    timeout: 10000,
-    transports: ["websocket", "polling"],
+    // withCredentials: true,
+    // reconnection: true,
+    // reconnectionAttempts: 5,
+    // reconnectionDelay: 1000,
+    // timeout: 10000,
+    // transports: ["websocket", "polling"],
+    withCredentials: true, // Allow cross-origin credential sharing
+    transports: ["websocket"], // Force WebSocket only
+    upgrade: false, // Prevent transport upgrades
+    reconnectionDelay: 500,
+    timeout: 5000,
   });
 };
